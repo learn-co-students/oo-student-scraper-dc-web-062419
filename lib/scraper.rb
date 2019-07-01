f8a925b6eb5c52538f6615d7733dfe6f0ae6673d
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-01 11:15:11 -0400

    Completed Lab 🤙

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,41 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css('div.student-card').each do |student|
+      students << {
+        :name => student.css('h4.student-name').text,
+        :location => student.css('p.student-location').text,
+        :profile_url => student.css('a').attribute('href').value
+      }
+    end
+    return students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    html = Nokogiri::HTML(open(profile_url))
+
+    student[:profile_quote] = html.css('.profile-quote').text if html.css('.profile-quote')
+    student[:bio] = html.css('div.description-holder p').text if html.css('.description-holder p')
+
+    links = html.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?('linkedin')
+        student[:linkedin] = link
+      elsif link.include?('twitter')
+        student[:twitter] = link
+      elsif link.include?('github')
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end
+    return student
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +6,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value| 
+      self.send("#{attribute}=", value)
+    end
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    # @twitter = student_hash[:twitter]
+    # @linkedin = student_hash[:linkedin]
+    # @github = student_hash[:github]
+    # @blog = student_hash[:blog]
+    # @profile_quote = student_hash[:profile_quote]
+    # @bio = student_hash[:bio]
   end
 
   def self.all
-    
+    @@all
   end
 end
 
